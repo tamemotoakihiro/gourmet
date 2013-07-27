4b6d8ee507e039b8e4365ae9f88427bedf748d82
chiastolite <chiastolite.1980@gmail.com>
2013-07-27 21:43:36 +0900

    generate user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130727123658) do
+ActiveRecord::Schema.define(version: 20130727124315) do
 
   create_table "reviews", force: true do |t|
     t.string   "content"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 20130727123658) do
   create_table "shops", force: true do |t|
     t.string   "name"
     t.string   "url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "mail"
+    t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
