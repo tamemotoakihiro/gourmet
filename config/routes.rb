8f09a3af9f9b681acdae496d547170854786b075
chiastolite <chiastolite.1980@gmail.com>
2013-07-28 10:24:51 +0900

    define routes for review

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Gourmet::Application.routes.draw do
+  resources :reviews
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
