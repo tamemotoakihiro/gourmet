bf4f7997a111c9390380883a739c92d09bea2d47
chiastolite <chiastolite.1980@gmail.com>
2013-07-28 11:19:14 +0900

    [from now] 2013/07/28 11:19:14
    diff --git a/app/controllers/sessions_controller.rb b/app/controllers/sessions_controller.rb
    index f8a2b61..0a85a2c 100644
    --- a/app/controllers/sessions_controller.rb
    +++ b/app/controllers/sessions_controller.rb
    @@ -1,8 +1,12 @@
     class SessionsController < ApplicationController
       def create
    -    require 'pry-remote'; binding.pry_remote
    +    user = User.from_omniauth(env["omniauth.auth"])
    +    session[:user_id] = user.id
    +    redirect_to root_url, notice: "Signed in!"
       end
    
       def destory
    +    session[:user_id] = nil
    +    redirect_to root_url, notice: "Signed out!"
       end
     end

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,12 @@
 class SessionsController < ApplicationController
   def create
-    require 'pry-remote'; binding.pry_remote
+    user = User.from_omniauth(env["omniauth.auth"])
+    session[:user_id] = user.id
+    redirect_to root_url, notice: "Signed in!"
   end
 
   def destory
+    session[:user_id] = nil
+    redirect_to root_url, notice: "Signed out!"
   end
 end
