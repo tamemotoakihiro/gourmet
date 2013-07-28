baf4b25dea4c6671e74263002156e0a2757f38dc
chiastolite <chiastolite.1980@gmail.com>
2013-07-28 11:50:25 +0900

    login only

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,6 @@
 class ReviewsController < ApplicationController
   before_action :set_review, only: [:show, :edit, :update, :destroy]
+  before_action :member_only
 
   # GET /reviews
   # GET /reviews.json
@@ -68,6 +69,9 @@ class ReviewsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_review
       @review = Review.find(params[:id])
+    end
+    def member_only
+      redirect_to '/auth/facebook' unless logged_in?
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
